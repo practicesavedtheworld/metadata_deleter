4148d894f476d06228dd11fd0ef3de2cef7a3363
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-10-23 15:06:54 +0300

    add constants

---
+++ src/constants/constants.py
@@ -0,0 +1,23 @@
+IMAGE_TYPES = (
+    'rgb',
+    'gif',
+    'pbm',
+    'pgm',
+    'ppm',
+    'tiff',
+    'rast',
+    'xbm',
+    'jpeg',
+    'bmp',
+    'png',
+    'webp',
+    'exr',
+    'svg',
+)
+
+COMMANDS = {
+    ("help", "-h"): "help",
+    ("change", "-c"): "change",
+    ("delete", "-d"): "delete",
+}
+print("help" in COMMANDS.keys())
