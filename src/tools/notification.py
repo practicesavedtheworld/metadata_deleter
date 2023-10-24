d81c908d1854647bc032106e671b5f6452ae5f8f
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-10-24 14:11:21 +0300

    add notify decorator

---
+++ src/tools/notification.py
@@ -0,0 +1,30 @@
+from typing import ParamSpec, TypeVar, Callable
+import functools
+
+from src.tools.print_colorful import print_colorful
+
+T = TypeVar("T")
+P = ParamSpec("P")
+
+
+def notify(operation: str) -> Callable[[Callable[P, T]], Callable[P, T | None]]:
+    """Based on |operation| parameter tells what exactly executed or failed"""
+
+    def notifier(func: Callable[P, T]) -> Callable[P, T | None]:
+        """Notifies user that something completed or went wrong"""
+
+        @functools.wraps(func)
+        def wrapper(*args: P.args, **kwargs: P.kwargs) -> T | None:
+            func_exc_result = None
+            try:
+                func_exc_result = func(*args, **kwargs)
+                print_colorful(f"{operation} executed successfully", "green")
+            except Exception as err:
+                # TODO log that
+                print_colorful(f"{operation} execution failed", "red")
+            finally:
+                return func_exc_result
+
+        return wrapper
+
+    return notifier
