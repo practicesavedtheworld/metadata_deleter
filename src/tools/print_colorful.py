907d04904917eade95729128eb7f1a4feae2c780
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-10-24 14:12:01 +0300

    add color printing opportunity

---
+++ src/tools/print_colorful.py
@@ -0,0 +1,17 @@
+def print_colorful(text: str, color: str) -> None:
+    colors = {
+        "black": "\033[30m",
+        "red": "\033[31m",
+        "green": "\033[32m",
+        "yellow": "\033[33m",
+        "blue": "\033[34m",
+        "magenta": "\033[35m",
+        "cyan": "\033[36m",
+        "white": "\033[37m",
+        "reset": "\033[0m"
+    }
+    if color in colors:
+        print(f"{colors[color]}{text}{colors['reset']}")
+    else:
+        print(text)
+
