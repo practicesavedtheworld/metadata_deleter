43aa09ee7c75ec26b036b0ac96b61e3a9f6c1f0e
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-10-25 11:04:51 +0300

    add method to base class

--- src/file_types/base_file_object.py
+++ src/file_types/base_file_object.py
@@ -14,4 +14,15 @@ class BaseFileObject(ABC):
     def present_metadata_dict(self):
         ...
 
+    @abstractmethod
+    def delete_all_metadata(self):
+        ...
 
+    @abstractmethod
+    def change_metadata_field(self, key: str, value: str) -> None:
+        ...
+
+    @abstractmethod
+
+    def save_file(self) -> None:
+        ...
