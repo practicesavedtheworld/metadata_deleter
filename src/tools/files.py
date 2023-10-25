a1d48e7dc8f9ff096ad1db5476a1b74288ea785d
practicesavedtheworld <richdaysiscoming@gmail.com>
2023-10-25 11:08:06 +0300

    update file tools

--- src/tools/files.py
+++ src/tools/files.py
@@ -2,7 +2,25 @@ import os
 import sys
 from pathlib import Path
 
+from PIL import Image, UnidentifiedImageError
+
+from src.constants import NOT_IMAGE
+
 ROOT = Path(__file__).parent.parent.parent
+
+
+def do_nothing(sig, sig_frame):
+    pass
+
+
+def get_image_format(path: str) -> str | NOT_IMAGE:
+    """Using PIL try opening file and define format."""
+    try:
+        img = Image.open(path)
+        return img.format
+    except UnidentifiedImageError:
+        return NOT_IMAGE
+
 
 def get_unique_filename(name: str, fmt: str) -> str:
     """Checks if the filename exists, if it does, appends an integer affix
